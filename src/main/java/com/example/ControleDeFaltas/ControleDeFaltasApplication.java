8b9cf3a9a0bd8ff1960ac1d5c708e8b90ed866d1
eikeszdev <eike2528@hotmail.com>
2024-07-27 19:36:34 -0300

    first commit

---
+++ src/main/java/com/example/ControleDeFaltas/ControleDeFaltasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControleDeFaltas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeFaltasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeFaltasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ControleDeFaltas/ControleDeFaltasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControleDeFaltas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeFaltasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
